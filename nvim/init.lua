797ded162845144b5701c90a126470116c21c646
Akbar <202290398@ajou.uz>
2025-12-29 18:42:40 +0500

    Fix Treesitter setup function call

--- nvim/init.lua
+++ nvim/init.lua
@@ -221,7 +221,7 @@ require("lazy").setup({
   { "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter").setup({
         ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "bash" },
         highlight = { enable = true },
         indent = { enable = true },
